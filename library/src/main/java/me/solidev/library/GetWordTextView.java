43d6c10486581780ae59f064c30941549e1c8f9d
burgessjp <1104160707@qq.com>
2016-11-09 17:38:31 +0800

    Initial commit

---
+++ library/src/main/java/me/solidev/library/GetWordTextView.java
@@ -0,0 +1,216 @@
+package me.solidev.library;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by _SOLID
+ * Date:2016/11/9
+ * Time:11:02
+ * <p>
+ * Desc:A TextView that can get every word in content.
+ * </p>
+ */
+
+public class GetWordTextView extends TextView {
+
+    private CharSequence mText;
+    private BufferType mBufferType;
+
+    private OnWordClickListener mOnWordClickListener;
+    private SpannableString mSpannableString;
+
+    private BackgroundColorSpan mSelectedBackSpan;
+    private ForegroundColorSpan mSelectedForeSpan;
+
+    private int highlightColor;
+    private String highlightText;
+    private int selectedColor;
+    private int language;//0:english,1:chinese
+
+    public GetWordTextView(Context context) {
+        this(context, null);
+    }
+
+    public GetWordTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public GetWordTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.GetWordTextView);
+        highlightColor = ta.getColor(R.styleable.GetWordTextView_highlightColor, Color.RED);
+        highlightText = ta.getString(R.styleable.GetWordTextView_highlightText);
+        selectedColor = ta.getColor(R.styleable.GetWordTextView_selectedColor, Color.BLUE);
+        language = ta.getInt(R.styleable.GetWordTextView_language, 0);
+        ta.recycle();
+    }
+
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        this.mText = text;
+        mBufferType = type;
+        setHighlightColor(Color.TRANSPARENT);
+        setMovementMethod(LinkMovementMethod.getInstance());
+        setText();
+    }
+
+    private void setText() {
+        mSpannableString = new SpannableString(mText);
+        //set highlight text
+        setHighLightSpan(mSpannableString);
+        //separate word
+        if (language == 0) {//deal english
+            dealEnglish();
+        } else {//deal chinese
+            dealChinese();
+        }
+        super.setText(mSpannableString, mBufferType);
+
+    }
+
+    private void dealChinese() {
+        //filter char(you can add more)
+        Character[] arr = new Character[]{',', '.', ';', '，', '。', '！', '；', '、', '：', '“', '”'};
+        List<Character> punctuations = Arrays.asList(arr);
+
+        for (int i = 0; i < mText.length(); i++) {
+            char ch = mText.charAt(i);
+            if (!punctuations.contains(ch)) {
+                mSpannableString.setSpan(getClickableSpan(), i, i + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
+        }
+    }
+
+    private void dealEnglish() {
+        List<Integer> wordIndices = getEnglishWordIndices();
+        int start = 0;
+        int end;
+        int indexSize = wordIndices.size();
+        for (int i = 0; i < indexSize + 1; i++) {
+            end = (i == indexSize ? mText.length() : wordIndices.get(i));
+            mSpannableString.setSpan(getClickableSpan(), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            start = end + 1;
+            if (end == mText.length()) {
+                break;
+            }
+        }
+    }
+
+    @NonNull
+    private List<Integer> getEnglishWordIndices() {
+        List<Integer> wordIndices = getWordIndices(mText.toString(), ' ');
+        wordIndices.addAll(getWordIndices(mText.toString(), ','));
+        wordIndices.addAll(getWordIndices(mText.toString(), '.'));
+        wordIndices.addAll(getWordIndices(mText.toString(), '!'));
+        wordIndices.addAll(getWordIndices(mText.toString(), ';'));
+        Collections.sort(wordIndices);
+        return wordIndices;
+    }
+
+    /**
+     * Get every word's index array of text
+     *
+     * @param text the content
+     * @param ch   separate char
+     * @return index array
+     */
+    private List<Integer> getWordIndices(String text, char ch) {
+        int pos = text.indexOf(ch);
+        List<Integer> indices = new ArrayList<>();
+        while (pos != -1) {
+            indices.add(pos);
+            pos = text.indexOf(ch, pos + 1);
+        }
+        return indices;
+    }
+
+    private void setHighLightSpan(SpannableString spannableString) {
+        if (TextUtils.isEmpty(highlightText)) {
+            return;
+        }
+        int hIndex = mText.toString().indexOf(highlightText);
+        while (hIndex != -1) {
+            spannableString.setSpan(new ForegroundColorSpan(highlightColor), hIndex, hIndex + highlightText.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            hIndex += highlightText.length();
+            hIndex = mText.toString().indexOf(highlightText, hIndex);
+        }
+    }
+
+    private void setSelectedSpan(TextView tv) {
+        if (mSelectedBackSpan == null || mSelectedForeSpan == null) {
+            mSelectedBackSpan = new BackgroundColorSpan(selectedColor);
+            mSelectedForeSpan = new ForegroundColorSpan(Color.WHITE);
+        } else {
+            mSpannableString.removeSpan(mSelectedBackSpan);
+            mSpannableString.removeSpan(mSelectedForeSpan);
+        }
+        mSpannableString.setSpan(mSelectedBackSpan, tv.getSelectionStart(), tv.getSelectionEnd(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mSpannableString.setSpan(mSelectedForeSpan, tv.getSelectionStart(), tv.getSelectionEnd(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        GetWordTextView.super.setText(mSpannableString, mBufferType);
+    }
+
+    public void dismissSelected() {
+        mSpannableString.removeSpan(mSelectedBackSpan);
+        mSpannableString.removeSpan(mSelectedForeSpan);
+        GetWordTextView.super.setText(mSpannableString, mBufferType);
+    }
+
+    private ClickableSpan getClickableSpan() {
+        return new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                TextView tv = (TextView) widget;
+                String word = tv
+                        .getText()
+                        .subSequence(tv.getSelectionStart(),
+                                tv.getSelectionEnd()).toString();
+                setSelectedSpan(tv);
+
+                if (mOnWordClickListener != null) {
+                    mOnWordClickListener.onClick(word);
+                }
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+            }
+        };
+    }
+
+
+    public void setOnWordClickListener(OnWordClickListener listener) {
+        this.mOnWordClickListener = listener;
+    }
+
+    public void setHighLightText(String text) {
+        highlightText = text;
+    }
+
+    public void setHighLightColor(int color) {
+        highlightColor = color;
+    }
+
+    public interface OnWordClickListener {
+        void onClick(String word);
+    }
+}
---
+++ sample/src/main/java/me/solidev/getwordtextview/MainActivity.java
@@ -0,0 +1,54 @@
+package me.solidev.getwordtextview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import me.solidev.library.GetWordTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GetWordTextView mEnglishGetWordTextView;
+    private GetWordTextView mChineseGetWordTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mEnglishGetWordTextView = (GetWordTextView) findViewById(R.id.english_get_word_text_view);
+        mEnglishGetWordTextView.setText("A view that can get every word inside,it's very helpful view!");
+        mEnglishGetWordTextView.setOnWordClickListener(new GetWordTextView.OnWordClickListener() {
+            @Override
+            public void onClick(String word) {
+                showToast(word);
+            }
+        });
+
+        mChineseGetWordTextView = (GetWordTextView) findViewById(R.id.chinese_get_word_text_view);
+        mChineseGetWordTextView.setText("这是一个中文句子的例子，这是一个中文句子的例子。");
+        mChineseGetWordTextView.setOnWordClickListener(new GetWordTextView.OnWordClickListener() {
+            @Override
+            public void onClick(String word) {
+                showToast(word);
+            }
+        });
+        findViewById(R.id.btn_dismiss_selected).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEnglishGetWordTextView.dismissSelected();
+                mChineseGetWordTextView.dismissSelected();
+            }
+        });
+    }
+
+    Toast toast;
+
+    public void showToast(String msg) {
+        if (toast == null) {
+            toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
+        }
+        toast.setText(msg);
+        toast.show();
+    }
+}
